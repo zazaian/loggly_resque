ad70d887930efb45a7b22346e3d062fcfcabdefe
Mike Zazaian <mike@zop.io>
2016-02-21 16:26:42 +0100

    add spec for LogglyResque, set Loggly tag suffix from LOGGLY_CONFIG in loggly initializer instead of setting it in /lib files

---
+++ loggly_resque_spec.rb
@@ -0,0 +1,10 @@
+require 'logglier'
+require_relative '../../lib/loggly_resque'
+require_relative '../toolkits/lib/loggly_resque/shared_examples'
+require_relative '../support/test_classes/lib/loggly_resque/loggly_resque_test'
+
+describe LogglyResque, type: :vendor_library do
+  include Toolkits::Lib::LogglyResqueToolkit::SharedExamples
+
+  it_behaves_like "the #logger is implemented through Logglier with LogglyResque", LogglyResqueTest
+end
