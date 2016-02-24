fa624613f8c723571273846ebad065f19b5f61b1
Mike Zazaian <mike@zop.io>
2016-02-24 16:36:31 +0100

    fix a bunch of rubocop formatting complaints in LogglyResque

--- loggly_resque.rb
+++ loggly_resque.rb
@@ -1,7 +1,7 @@
-# when extending a class that is passed directly into Resque for the purposes of handling
-# the Class#perform method, this will enable that class to log directly to Loggly if the #logger
-# method is used to call the logger within the perform block
-
+# when extending a class that is passed directly into Resque for the purposes
+# of handling the Class#perform method, this will enable that class to log
+# directly to Loggly if the #logger method is used to call the logger within
+# the perform block
 module LogglyResque
   def logger
     @logger ||= Logglier.new(logger_url, format: :json)
@@ -11,7 +11,7 @@ module LogglyResque
   # https://logs-01.loggly.com/inputs/<loggly-token>/tag/tag-name
   #
   def logger_url
-    [ logger_base_url, ENV['LOGGLY_TOKEN'], "tag", queue_tag_name ].join("/")
+    [logger_base_url, ENV['LOGGLY_TOKEN'], "tag", queue_tag_name].join("/")
   end
 
   def logger_base_url
@@ -19,6 +19,6 @@ module LogglyResque
   end
 
   def queue_tag_name
-    [ @queue.to_s.gsub(/_+/,'-'), LOGGLY_CONFIG['TAG_SUFFIX'] ].join("-")
+    [@queue.to_s.gsub(/_+/, '-'), LOGGLY_CONFIG['TAG_SUFFIX']].join("-")
   end
 end
