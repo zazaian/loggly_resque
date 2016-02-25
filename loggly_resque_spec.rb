0e911d7288b2c6c72257d1d5aa01aa6f83734e5a
Mike Zazaian <mike@zop.io>
2016-02-25 10:50:53 +0100

    convert all single quotes to double quotes in new lib and spec files

--- loggly_resque_spec.rb
+++ loggly_resque_spec.rb
@@ -1,7 +1,7 @@
-require 'logglier'
-require_relative '../../lib/loggly_resque'
-require_relative '../toolkits/lib/loggly_resque/shared_examples'
-require_relative '../support/test_classes/lib/loggly_resque/loggly_resque_test'
+require "logglier"
+require_relative "../../lib/loggly_resque"
+require_relative "../toolkits/lib/loggly_resque/shared_examples"
+require_relative "../support/test_classes/lib/loggly_resque/loggly_resque_test"
 
 describe LogglyResque, type: :vendor_library do
   include Toolkits::Lib::LogglyResqueToolkit::SharedExamples
